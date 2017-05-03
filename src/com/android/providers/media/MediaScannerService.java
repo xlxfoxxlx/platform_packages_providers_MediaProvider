3aa1cc3dc6a804f0b3e025f5ca58a091ef90e728
Akhil Narang <akhilnarang.1999@gmail.com>
2017-05-03 20:47:03 +0200

    Revert "MediaProvider: Support to scan carrier's media folder"
      Also cleaned up whitespaces I saw
    
    This reverts commit d5d93589e15a251d4850b69caaebcd8633dd65c9.
    
    Change-Id: Id690b9207021f166a9e58958a74160dff61ba796

--- src/com/android/providers/media/MediaScannerService.java
+++ src/com/android/providers/media/MediaScannerService.java
@@ -38,12 +38,10 @@ import android.os.storage.StorageManager;
 import android.provider.MediaStore;
 import android.util.Log;
 
-import com.android.internal.os.RegionalizationEnvironment;
 import com.android.internal.util.ArrayUtils;
 
 import java.io.File;
 import java.util.Arrays;
-import java.util.List;
 
 public class MediaScannerService extends Service implements Runnable {
     private static final String TAG = "MediaScannerService";
@@ -52,7 +50,7 @@ public class MediaScannerService extends Service implements Runnable {
     private volatile ServiceHandler mServiceHandler;
     private PowerManager.WakeLock mWakeLock;
     private String[] mExternalStoragePaths;
-    
+
     private void openDatabase(String volumeName) {
         try {
             ContentValues values = new ContentValues();
@@ -236,24 +234,6 @@ public class MediaScannerService extends Service implements Runnable {
                                 Environment.getRootDirectory() + "/media",
                                 Environment.getOemDirectory() + "/media",
                         };
-
-                        if (RegionalizationEnvironment.isSupported()) {
-                            final List<File> regionalizationDirs = RegionalizationEnvironment
-                                    .getAllPackageDirectories();
-                            if (regionalizationDirs.size() > 0) {
-                                String[] mediaDirs =
-                                        new String[directories.length + regionalizationDirs.size()];
-                                for (int i = 0; i < directories.length; i++) {
-                                    mediaDirs[i] = directories[i];
-                                }
-                                int j = directories.length;
-                                for (File f : regionalizationDirs) {
-                                    mediaDirs[j] = f.getAbsolutePath() + "/system/media";
-                                    j++;
-                                }
-                                directories = mediaDirs;
-                            }
-                        }
                     }
                     else if (MediaProvider.EXTERNAL_VOLUME.equals(volume)) {
                         // scan external storage volumes
